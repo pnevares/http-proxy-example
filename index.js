0852c1b3814cdba3f3ac389b3458a680d95a9406
Pablo Nevares <pablo@nevares.com>
2015-02-14 15:22:19 -0700

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var url = require('url');
+
+var config = {
+    port: 8080,
+}
+
+var server = http.createServer(function(serverRequest, serverResponse) {
+    var destination = 'http://' + serverRequest.url.slice(1);
+
+    console.log(destination);
+    http.get(destination, function(clientResponse) {
+        clientResponse.on('data', function(chunk) {
+            serverResponse.write(chunk);
+        });
+        clientResponse.on('end', function() {
+            serverResponse.end();
+        });
+    }).on('error', function(error) {
+        console.log('clientResponse error: ' + error);
+    });
+});
+
+server.listen(config.port, function() {
+    console.log('listening on port %s', config.port);
+});
